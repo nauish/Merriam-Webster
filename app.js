ed2f76b3742f99bc1fe8287ec6ce1b82bcadcd7d
Rick <101254647+nauish@users.noreply.github.com>
2023-09-07 02:58:29 +0800

    Let user input API key as second arg

--- app.js
+++ app.js
@@ -1,28 +1,43 @@
-const https = require('https');
-function getData(term) {
-    try {
-        // Request Data
-        const request = https.get(
-            `https://dictionaryapi.com/api/v3/references/collegiate/json/${term}?key=`,
-             (response) => {
-                let body = '';
-                // console.dir(response.statusCode);
-                response.on('data', (data) => {
-                    body += data.toString();
-                });
-                // Parse Data
-                response.on('end', () => {
-                    const def = JSON.parse(body);
-                    // Format Data
-                    // Return Data
-                    console.log(def[0].shortdef)
-                })
-             });
-    }
-    catch (error){
-        console.error(error.message);
-    }
+/* eslint-disable no-undef */
+const https = require("https");
+
+function printErr(error) {
+  console.error(error.message);
 }
 
-const query = process.argv.slice(2);
-query.forEach(getData);
+function getDefinition(term) {
+  try {
+    // Request Data
+    const request = https.get(
+      `https://dictionaryapi.com/api/v3/references/collegiate/json/${term}?key=${apiKey}`,
+      (response) => {
+        let responseBody = "";
+        // console.dir(response.statusCode);
+        response.on("data", (data) => {
+          responseBody += data.toString();
+        });
+        // Parse Data
+        try {
+          response.on("end", () => {
+            const definition = JSON.parse(responseBody);
+            // Format Data
+            // Return Data
+            console.log(definition[0].shortdef);
+          });
+        } catch (error) {
+          printErr(error);
+        }
+      }
+    );
+    request.on("error", (error) => printErr(error));
+  } catch (error) {
+    printErr(error);
+  }
+}
+
+const query = process.argv.slice(3);
+let apiKey = process.argv[2];
+if (apiKey.length !== 36) {
+  console.error("Please enter a valid API key!");
+}
+query.forEach(getDefinition);
